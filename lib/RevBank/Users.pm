3a07b8eadbc48ae82d863b6af0ee7c845280a5e0
Juerd Waalboer <juerd@tnx.nl>
2022-06-12 02:17:44 +0200

    Use string amounts for balance
    Doesn't change anything in practice

--- lib/RevBank/Users.pm
+++ lib/RevBank/Users.pm
@@ -24,7 +24,7 @@ sub names() {
 }
 
 sub balance($username) {
-    return RevBank::Amount->new_from_float( _read()->{ lc $username }->[1] );
+    return RevBank::Amount->parse_string( _read()->{ lc $username }->[1] );
 }
 
 sub since($username) {
