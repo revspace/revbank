revspace/revbank

commit f2d09b4da53419b5690a6f3cf7d39fdc61221e42

v6.1.1: Feature: warning messages for invalid accounts

lib/RevBank/Users.pm
@@ -13,7 +13,20 @@ my $filename = "revbank.accounts";
 
 sub _read() {
     my @users;
-    /\S/ and push @users, [split " "] for slurp $filename;
+    for my $line (slurp $filename) {
+        $line =~ /\S/ or next;
+        # Not using RevBank::Prompt::split_input to keep parsing by external
+        # scripts simple, since so many such scripts exist.
+
+        my @split = split " ", $line;
+
+        if ($split[1] =~ /^!/) {
+            # Special case: use rest of the line (see POD).
+            @split = split " ", $line, 2;
+        }
+
+        push @users, \@split;
+    }
 
     my %users;
     for (@users) {
@@ -103,24 +116,31 @@ sub parse_user($username, $allow_invalid = 0) {
 
     my $users = _read();
 
-    exists $users->{ lc $username }
-        or return undef;
+    my $user = $users->{ lc $username } or return undef;
+
+    if ($user->[1] =~ /^!(.*)/) {
+        warn "$username: Invalid account ($1).\n";
+    }
 
     $allow_invalid or defined balance($username)
         or return undef;
 
-    return $users->{ lc $username }->[0];
+    return $user->[0];
 }
 
 sub assert_user($username) {
     my $users = _read();
 
-    return exists $users->{ lc $username }
-        ? $users->{ lc $username }->[0]
-        : (is_hidden($username)
-            ? create($username)
-            : Carp::croak("No such user ($username)")
-        );
+    my $user = $users->{ lc $username };
+
+    if ($user) {
+        Carp::croak("Account $username can't be used") if not balance $username;
+        return $user->[0];
+    }
+
+    return create $username if is_hidden $username;
+
+    Carp::croak("No such user ($username)")
 }
 
 1;

revbank
@@ -16,7 +16,7 @@ use RevBank::Messages;
 use RevBank::Cart;
 use RevBank::Prompt;
 
-our $VERSION = "6.0.5";
+our $VERSION = "6.1.1";
 our %HELP1 = (
     "abort" => "Abort the current transaction",
 );

lib/RevBank/Users.pod
@@ -80,6 +80,10 @@ Every account name must be unique. A file with duplicate names is not valid and 
 
 The account balance is a number with two decimal digits. Positive numbers may have a C<+> sign. Negative number have a C<-> sign.
 
+If the value in this field is not a valid number, the account is treated as non-existent by most of RevBank, while still being unavailable for C<adduser>.
+
+If the value begins with a C<!> character, the I<rest of the line> is taken as a description of why the account name is not available and printed as a warning when the account name is used.
+
 =item * Last use timestamp
 
 Local datetime of the last update of this account.

plugins/adduser
@@ -24,9 +24,6 @@ sub username($self, $cart, $name, @) {
     return REJECT, "Sorry, that's too numeric to be a user name."
         if defined parse_amount($name);
 
-    return REJECT, "That name already exists."
-        if defined parse_user($name);
-
     return REJECT, "That name is not available."
         if defined parse_user($name, 1);
 

plugins/products
@@ -27,11 +27,7 @@ sub read_products() {
 
         my @split = RevBank::Prompt::split_input($line);
 
-        if (grep /\0SEPARATOR/, @split) {
-            warn "Invalid character in $filename line $linenr.\n";
-            next;
-        }
-        if (grep /\0/, @split) {
+        if (not @split or ref $split[0] or grep /\0/, @split) {
             warn "Invalid value in $filename line $linenr.\n";
             next;
         }

commit e3b054272d4e3a14f1b42066b4f1774b4169db8e

v6.1.4: fix bug (0.00 balance account not usable)
Introduced in f2d09b4d

lib/RevBank/Users.pm
@@ -134,7 +134,7 @@ sub assert_user($username) {
     my $user = $users->{ lc $username };
 
     if ($user) {
-        Carp::croak("Account $username can't be used") if not balance $username;
+        Carp::croak("Account $username can't be used") if not defined balance $username;
         return $user->[0];
     }
 

revbank
@@ -16,7 +16,7 @@ use RevBank::Messages;
 use RevBank::Cart;
 use RevBank::Prompt;
 
-our $VERSION = "6.1.3";
+our $VERSION = "6.1.4";
 our %HELP1 = (
     "abort" => "Abort the current transaction",
 );